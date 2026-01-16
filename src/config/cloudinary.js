c0aa2a305df3f4db0b7fc64ecddd51e3ea2ca816
aayush <ar0671362@gmail.com>
2026-01-16 15:53:55 +0530

    cloudinary signature issue

--- src/config/cloudinary.js
+++ src/config/cloudinary.js
@@ -1,12 +1,26 @@
+// const cloudinary = require('cloudinary').v2;
+
+// const configureCloudinary = () => {
+//   cloudinary.config({
+//     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+//     api_key: process.env.CLOUDINARY_API_KEY,
+//     api_secret: process.env.CLOUDINARY_API_SECRET
+//   });
+
+//   return cloudinary;
+// };
+
+// module.exports = configureCloudinary;
 const cloudinary = require('cloudinary').v2;
 
 const configureCloudinary = () => {
   cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+    signature_algorithm: 'sha256'  // Add this line
   });
-
+  
   return cloudinary;
 };
 
